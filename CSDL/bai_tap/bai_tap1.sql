056619b6ebbda631f03cd1691634930d8d8b77fc
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-06-13 13:53:31 +0700

    up

--- CSDL/bai_tap/bai_tap1.sql
+++ CSDL/bai_tap/bai_tap1.sql
@@ -1,8 +1,10 @@
--- CREATE DATABASE `bai_tap1`
+ CREATE DATABASE `bai_tap1`;
+ 
 CREATE TABLE `bai_tap1`.class(
 `id` INT NOT NULL,
 `name` VARCHAR(45) NULL,
 PRIMARY KEY (`id`) );
+
 CREATE TABLE `bai_tap1`.teacher(
 `id` INT NOT NULL,
 `name` VARCHAR(45) NULL,
