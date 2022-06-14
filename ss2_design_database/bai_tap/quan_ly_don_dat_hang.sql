1cf665ba2ac7a815f671a48fe28e0085f9ead6e0
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-06-14 11:14:59 +0700

    home work

--- ss2_design_database/bai_tap/quan_ly_don_dat_hang.sql
+++ ss2_design_database/bai_tap/quan_ly_don_dat_hang.sql
@@ -43,11 +43,17 @@ PRIMARY KEY (ma_vtu, so_dh),
 FOREIGN KEY (ma_vtu) REFERENCES vat_tu(ma_vtu),
 FOREIGN KEY (so_dh) REFERENCES don_dh(so_dh));
 
+CREATE TABLE sdt(
+id_sdt INT AUTO_INCREMENT PRIMARY KEY,
+sdt VARCHAR(11) NOT NULL
+);
+
 CREATE TABLE nha_cc(
 ma_nha_cc INT,
 so_dh INT,
 ten_nha_cc VARCHAR(50) NOT NULL,
 dia_chi VARCHAR(50) NOT NULL,
-sdt VARCHAR(11) NOT NULL,
+id_sdt INT,
 PRIMARY KEY (ma_nha_cc, so_dh),
-FOREIGN KEY (so_dh) REFERENCES don_dh(so_dh));
+FOREIGN KEY (so_dh) REFERENCES don_dh(so_dh),
+FOREIGN KEY (id_sdt) REFERENCES sdt(id_sdt));
