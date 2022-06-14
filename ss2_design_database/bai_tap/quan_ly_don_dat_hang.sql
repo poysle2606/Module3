b1109dd3c16a1a319e0e0960f51f2c7f842727f3
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-06-14 11:19:49 +0700

    home work

--- ss2_design_database/bai_tap/quan_ly_don_dat_hang.sql
+++ ss2_design_database/bai_tap/quan_ly_don_dat_hang.sql
@@ -3,16 +3,16 @@ CREATE DATABASE quan_ly_don_dat_hang;
 use quan_ly_don_dat_hang;
 
 CREATE TABLE phieu_xuat(
-so_px INT AUTO_INCREMENT PRIMARY KEY,
+so_px     INT AUTO_INCREMENT PRIMARY KEY,
 ngay_xuat DATE);
 
 CREATE TABLE vat_tu(
-ma_vtu INT AUTO_INCREMENT PRIMARY KEY,
+ma_vtu  INT AUTO_INCREMENT PRIMARY KEY,
 ten_vtu VARCHAR(50) NOT NULL);
 
 CREATE TABLE chi_tiet_phieu_xuat(
-so_px INT,
-ma_vtu INT,
+so_px   INT,
+ma_vtu  INT,
 dg_xuat INT,
 sl_xuat INT,
 PRIMARY KEY(so_px, ma_vtu),
@@ -24,9 +24,9 @@ so_pn INT AUTO_INCREMENT PRIMARY KEY,
 ngay_nhap DATE);
 
 CREATE TABLE chi_tiet_phieu_nhap(
-ma_vtu INT,
-so_pn INT,
-dg_nhap DOUBLE,
+ma_vtu  INT,
+so_p  n INT,
+dg_nhap INT,
 sl_nhap INT,
 PRIMARY KEY(ma_vtu, so_pn),
 FOREIGN KEY (ma_vtu) REFERENCES vat_tu(ma_vtu),
@@ -37,8 +37,8 @@ so_dh INT AUTO_INCREMENT PRIMARY KEY,
 ngay_dh DATE);
 
 CREATE TABLE chi_tiet_dat_don_hang(
-ma_vtu INT,
-so_dh INT,
+ma_vtu  INT,
+so_dh   INT,
 PRIMARY KEY (ma_vtu, so_dh),
 FOREIGN KEY (ma_vtu) REFERENCES vat_tu(ma_vtu),
 FOREIGN KEY (so_dh) REFERENCES don_dh(so_dh));
@@ -49,11 +49,11 @@ sdt VARCHAR(11) NOT NULL
 );
 
 CREATE TABLE nha_cc(
-ma_nha_cc INT,
-so_dh INT,
+ma_nha_cc  INT,
+so_dh      INT,
 ten_nha_cc VARCHAR(50) NOT NULL,
-dia_chi VARCHAR(50) NOT NULL,
-id_sdt INT,
+dia_chi    VARCHAR(50) NOT NULL,
+id_sdt     INT,
 PRIMARY KEY (ma_nha_cc, so_dh),
 FOREIGN KEY (so_dh) REFERENCES don_dh(so_dh),
 FOREIGN KEY (id_sdt) REFERENCES sdt(id_sdt));
