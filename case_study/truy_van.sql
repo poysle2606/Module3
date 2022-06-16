7580139e89390d3b6d8e58488a638a6495d815a1
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-06-16 13:41:36 +0700

    up

--- case_study/truy_van.sql
+++ case_study/truy_van.sql
@@ -1,4 +1,4 @@
-use case_study;
+USE case_study;
 
 							 -- task 2--
 
@@ -7,16 +7,16 @@ WHERE(ho_ten LIKE 'H%' OR ho_ten LIKE 'K%' OR ho_ten LIKE 'T%' ) AND char_length
 
                              -- task 3--
 SELECT * FROM khach_hang 
-WHERE ((YEAR(CURDATE()) - YEAR(ngay_sinh)) BETWEEN 18 and 50) AND (dia_chi LIKE '%Đà Nẵng' OR dia_chi LIKE '%Quảng Trị');
+WHERE ((YEAR(CURDATE()) - YEAR(ngay_sinh)) BETWEEN 18 AND 50) AND (dia_chi LIKE '%Đà Nẵng' OR dia_chi LIKE '%Quảng Trị');
 
                              -- task 4--
-select kh.ma_khach_hang, kh.ho_ten , lk.ten_loai_khach, count(kh.ma_khach_hang) as so_lan_dat_phong
-from loai_khach lk
-join khach_hang kh on lk.ma_loai_khach = kh.ma_loai_khach
-join hop_dong hd on kh.ma_khach_hang = hd.ma_khach_hang
-where lk.ma_loai_khach = 1
-group by kh.ma_khach_hang
+SELECT kh.ma_khach_hang, kh.ho_ten , lk.ten_loai_khach, count(kh.ma_khach_hang) AS so_lan_dat_phong
+FROM loai_khach lk
+JOIN khach_hang kh ON lk.ma_loai_khach = kh.ma_loai_khach
+JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+WHERE lk.ma_loai_khach = 1
+GROUP BY kh.ma_khach_hang
 
                              -- task 5--
                              
-                             
+                              
