67a264a00b85992cf489821ed0307ee898811f8b
Call me my name: Boy <101506391+poysle2606@users.noreply.github.com>
2022-06-16 16:33:25 +0700

    up

--- case_study/truy_van.sql
+++ case_study/truy_van.sql
@@ -15,8 +15,38 @@ FROM loai_khach lk
 JOIN khach_hang kh ON lk.ma_loai_khach = kh.ma_loai_khach
 JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
 WHERE lk.ma_loai_khach = 1
-GROUP BY kh.ma_khach_hang
+GROUP BY kh.ma_khach_hang;
 
                              -- task 5--
-                             
+SELECT kh.ma_khach_hang, kh.ho_ten, lk.ten_loai_khach, hd.ma_hop_dong, dv.ten_dich_vu, hd.ngay_lam_hop_dong, hd.ngay_ket_thuc, 
+ifnull(dv.chi_phi_thue + hdct.so_luong * dvdk.gia,0) AS tong_tien 
+FROM loai_khach lk
+left JOIN khach_hang kh ON lk.ma_loai_khach = kh.ma_loai_khach
+left JOIN hop_dong hd ON kh.ma_khach_hang = hd.ma_khach_hang
+left JOIN dich_vu dv ON hd.ma_dich_vu = dv.ma_dich_vu
+left JOIN hop_dong_chi_tiet hdct ON hd.ma_hop_dong = hdct.ma_hop_dong
+left JOIN dich_vu_di_kem dvdk ON hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem
+GROUP BY ma_khach_hang
+ORDER BY ma_khach_hang;
+
+                             -- task 6 --
+SELECT dv.ma_dich_vu, dv.ten_dich_vu, dv.dien_tich, dv.chi_phi_thue, ldv.ten_loai_dich_vu FROM dich_vu dv
+JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+JOIN hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+group by ma_dich_vu
+having dv.ma_dich_vu NOT IN                             
+(SELECT dv.ma_dich_vu FROM dich_vu dv
+JOIN loai_dich_vu ldv ON dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
+JOIN hop_dong hd ON dv.ma_dich_vu = hd.ma_dich_vu
+WHERE (MONTH(hd.ngay_lam_hop_dong) BETWEEN 1 AND 3) AND YEAR(hd.ngay_lam_hop_dong) = 2021)
+ORDER BY chi_phi_thue DESC; 	
+
+                             -- task 7 --	
+
+                             -- task 8--
+-- cách 1 --                             
+SELECT distinct ho_ten FROM khach_hang;
+-- cách 2 --
+SELECT ho_ten FROM khach_hang
+GROUP BY ho_ten;							
                               
